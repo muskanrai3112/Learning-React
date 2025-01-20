f765604f66fb79f95eae742d5a096e8234d64ff0
Muskan Kumari <muskanofficial3112@gmail.com>
2025-01-20 19:59:55 +0530

    new changes

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,14 +1,15 @@
 import RestaurantCard from "./RestaurantCards";
 import reslist from "../../utlis/mockdata";
 import { useEffect, useState } from "react";
+import { searchIcon } from "../../images/Search_Icon.svg.png";
 
 const Body = () => {
   const [resFilter, setResFilter] = useState([]);
 
   useEffect(() => {
-    setTimeout(()=>{
+    setTimeout(() => {
       fetchData();
-    }, 200)
+    }, 200);
   }, []);
   const fetchData = async () => {
     try {
@@ -16,69 +17,75 @@ const Body = () => {
         "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       );
       const json = await response.json();
-      setResFilter(json?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle?.info|| [])
-      console.log(json?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle, "ggg")
-      console.log(setResFilter, "setResFilter")
-      console.log(json, "XXXXX");
-      // console.log(json?.resData?.resData?.info?.avgRatingString, "data0");
-      console.log(json?.data, "data1");
-      console.log(json?.data?.cards[2], "data2");
-      console.log(json?.data?.cards[2]?.card, "data3");
-      console.log(json?.data?.cards[2]?.card?.card, "data4");
-      console.log(json?.data?.cards[2]?.card?.card?.title, "data5");
-      console.log(json?.tid, "data6");
-      console.log(json?.data?.cards[2]?.card?.card, "data7");
+      console.log(json , "arun")
 
+      setResFilter(
+        json?.data?.cards[1].card.card.gridElements.infoWithStyle.restaurants
+      );
     } catch (error) {
       console.error("this is some error", error);
     }
   };
+  console.log(resFilter, "resFilterresFilter");
 
   // conditional rendering.....
-//   if( resFilter.length===0){
-//     return<>
-//   <div id="wrapper">		
-//   <div id="corpus"></div>
-// 	<div id="spinner"></div>
-// </div>
-// <div id="text">&nbsp;Loading ...</div>
-//      </>
-    
-//   }
+  //   if( resFilter.length===0){
+  //     return<>
+  //   <div id="wrapper">
+  //   <div id="corpus"></div>
+  // 	<div id="spinner"></div>
+  // </div>
+  // <div id="text">&nbsp;Loading ...</div>
+  //      </>
 
-  return resFilter.length===0? <><div id="wrapper">		
-  <div id="corpus"></div>
-	<div id="spinner"></div>
+  //   }
+
+  return resFilter.length === 0 ? (
+    <>
+   <div className="loader-main">
+   <div class="loader">
+  <svg viewBox="0 0 200 200">
+    <circle cx="100" cy="100" r="50"></circle>
+  </svg>
+  <svg viewBox="0 0 200 200">
+    <circle cx="100" cy="100" r="50"></circle>
+  </svg>
+  <svg viewBox="0 0 200 200">
+    <circle cx="100" cy="100" r="50"></circle>
+  </svg>
 </div>
-<div id="text">&nbsp;Loading ...</div></>: (
+    </div>
+    </>
+  ) : (
     <div className="contentBody">
       <div className="filter">
-    <div>
-    <button
-          className="fliter-btn"
-          onClick={() => {
-            const filterList = resFilter.filter(
-              (res) => res?.info?.avgRating > 4
-            );
-            setResFilter(filterList);
-            console.log(filterList,"filterList");
-            // some how i have to write filter logic here!
-          }}
-        >
-          Top Rated Restaurant
-        </button>
-    </div>
-    <div className="search">
-    <input type="text" placeholder="search"/>
-      <button className="fliter-btn">
-       search
-      </button>
-    </div>
+        <div>
+          <button
+            className="fliter-btn"
+            onClick={() => {
+              const filterList = resFilter.filter(
+                (res) => res?.info?.avgRating > 4
+              );
+              setResFilter(filterList);
+              console.log(filterList, "filterList");
+              // some how i have to write filter logic here!
+            }}
+          >
+            Top Rated Restaurant
+          </button>
+        </div>
+        <div className="search">
+          <input type="text" placeholder="search" />
+          <button className="fliter-btn">search</button>
+        </div>
       </div>
       <div className="res-container">
         <div className="res-cards">
-          {resFilter.map((restaurant, index) => (
-            <RestaurantCard key={index} resData={restaurant}/>
+        {console.log(resFilter, "resFilter - entire array")} 
+          {resFilter.map((restaurant, index) =>(
+          
+
+            <RestaurantCard key={index} resData={restaurant} />
           ))}
         </div>
       </div>
--- src/component/RestaurantCards.js
+++ src/component/RestaurantCards.js
@@ -1,34 +1,31 @@
-
-import {CARD_IMG} from "../../utlis/constant"
+import { CARD_IMG } from "../../utlis/constant";
 const RestaurantCard = (resData) => {
-  
-console.log(resData,"resData")
+  console.log(resData.resData, "resData");
 
   return (
-    resData && resData.resData && (
-    
-    <div className="res-card">
-      {console.log(resData.resData,"resDataresDataresData")}
-      <div className="res-card-single">
-        <div className="res-img">
-          <img
-            className="res-image"
-            src={CARD_IMG + resData.resData.imageId}
-            alt="res-image"
-          />
-        </div>
+    resData &&
+    resData.resData && (
+      <div className="res-card">
+        <div className="res-card-single">
+          <div className="res-img">
+            <img
+              className="res-image"
+              src={CARD_IMG + resData?.resData?.info?.cloudinaryImageId}
+              alt="res-image"
+            />
+          </div>
 
-        <div>
-          <h3>{resData?.resData?.info?.name}</h3>
-          <h4>{resData.cuisines}</h4>
-          <h4 className="star-rating">
-            {resData?.resData?.info?.avgRating}stars
-          </h4>
-          <h4>{resData?.resData?.info?.sla?.slaString}</h4>
-          <h4>{resData?.resData?.info?.cuisines.join(", ")}</h4>
+          <div>
+            <h3>{resData?.resData?.info?.name}</h3>
+            <h4>{resData.cuisines}</h4>
+            <h4 className="star-rating">
+              {resData?.resData?.info?.avgRating}stars
+            </h4>
+            <h4>{resData?.resData?.info?.sla?.slaString}</h4>
+            <h4>{resData?.resData?.info?.cuisines.join(", ")}</h4>
+          </div>
         </div>
       </div>
-    </div>
     )
   );
 };
